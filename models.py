1aa8bf5afd070874b92c6126c90abcaa8383d8a3
Pengkanlai <97509686+Pengkanlai@users.noreply.github.com>
2022-03-19 20:59:06 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
 
-def linear(X,W):
-    return X@W.t()
-
-def diagonal_linear(X, W_1, W_2):
-    return (W_1*W_2)@X
+def train_model(model,X,W_1,W_2):
+    if model == 'linear':
+        f = X @ W_1.t()
+    elif model == 'diagonal_linear':
+        f = (W_1*W_2)@X
+    return f
