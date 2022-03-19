468d2b8d23f1820cfc3b5bfe4430a1bc414a250f
Pengkanlai <97509686+Pengkanlai@users.noreply.github.com>
2022-03-19 22:22:39 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,58 +1,28 @@
 import torch
 import numpy as np
 
+nb_iterations = 100
+
 def train_model(model,X):
     if model == 'linear':
         f = linear(X)
     elif model == 'diagonal_linear':
         f = diagonal_linear(X)
-    return f
+    
 
-def mean_squared_loss(y, y_pred):
-    msl = pow(y_pred-y,2).mean()
-    return msl
-
-def gradient_MSL_linear_model(X, y, y_pred):
-    grad = -2*X*(y_pred-y)
-    return grad  
+def loss(type,y,y_pred):
+    if type == 'mean_squared':
+        loss = pow(y_pred-y,2).mean()
+    elif type == 'hinge':
+        loss = max(0, 1-torch.sign(y)*y_pred).mean()
   
-def gradient_MSL_diagonal_linear_model():
-  
-def run_SGD_linear_model_regression(X, y, learning_rate, nb_iterations, batch_size):
-    W = torch.random.normal(0., 1., size = (len(X[0])))
-    y_pred = model_linear(X,W)
-    train_loss = []
+def SGD_step(f, xtr, ytr, dt, bs, loss_type):
     
-    for i in range(nb_iterations):
-        grad = 0
-        for mu in range(len(y)):
-            grad += gradient_MSL_linear_model(X[mu], y[mu], y_pred[mu])
-        W -= learning_rate * grad 
-        
-        y_pred = model_linear(X,W) 
-        train_loss[i] = mean_squared_loss(y, y_pred)
-        
-    return W  
-  
-  def hinge_loss(y,y_pred):
-    hinge_loss = (1 - torch.sign(max(0,y*y_pred))).mean()
-    return hinge_loss
-  
-  def gradient_hinge_loss():
     
-  def run_SGD_diagonal_linear_model_regression(X, y, learning_rate, nb_iterations, batch_size):
-    W_1 = torch.random.normal(0., 1., size = (len(X[0])))
-    W_2 = torch.random.normal(0., 1., size = (len(X[0])))
-    y_pred = model_diagonal_linear(X, W_1, W_2)
-    train_loss = []
     
-    for i in range(nb_iterations):
-        grad = 0
-        for mu in range(len(y)):
-            grad += gradient_MSL_diagonal_linear_model(X[mu], y[mu], y_pred[mu])
-        W -= learning_rate * grad 
-        
-        y_pred = model_diagonal_linear(X, W_1, W_2)
-        train_loss[i] = mean_squared_loss(y, y_pred)
-        
-    return W     
+    
+ 
+  
+ 
+    
+  
