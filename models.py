6b518a4aa0b535963efbc15aca7114ead53395ad
Pengkanlai <97509686+Pengkanlai@users.noreply.github.com>
2022-03-19 20:33:54 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 import torch
 import numpy as np
 
-def model_linear(X,W):
-    return X @ W.t()
+def linear(X,W):
+    return X@W.t()
 
-def model_diagonal_linear(X, W_1, W_2):
-    return torch.mul(W_1,W_2.t()).mean()*X
+def diagonal_linear(X, W_1, W_2):
+    return (W_1*W_2)@X
