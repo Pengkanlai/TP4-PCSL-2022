fd5bb0c974db37b7b77a1db63327b58f67fc2efb
Antonio Sclocchi <antoniosclocchi@gmail.com>
2022-03-24 12:02:04 +0000

    Modified models and sgd

--- models.py
+++ models.py
@@ -1,8 +1,32 @@
+from audioop import bias
 import torch
-import numpy as np
+import torch.nn as nn
 
-def linear(x,w):
-    return x @ w
 
-def diagonal_linear(x,w_1,w_2):
-    return (w_1*w_2)@x
+class linear(nn.Module):
+    def __init__(self, d, bias=False):
+        super(linear, self).__init__()
+        if bias:
+            self.linear_layer = nn.Linear(d, 1, bias=True) # an affine operation: y = Wx + b
+        else:
+            self.linear_layer = nn.Linear(d, 1, bias=False) # an homogenous linear operation: y = Wx
+
+    def forward(self, x):
+        x = self.linear_layer(x)
+        return x
+
+
+class diagonal_linear(nn.Module):
+    def __init__(self, d):
+        super(linear, self).__init__()
+        self.linear1 = nn.Linear(d, 1, bias=False)
+        self.linear2 = nn.Linear(d, 1, bias=False)
+        self.linear3 = nn.Linear(d, 1, bias=False)
+        for p1, p2, p3 in zip(self.linear1.parameters(), self.linear2.parameters(), self.linear3.paramteres()):
+            p3 = p1*p2
+
+    def forward(self, x):
+        for p1, p2, p3 in zip(self.linear1.parameters(), self.linear2.parameters(), self.linear3.paramteres()):
+            p3 = p1*p2
+        x = self.linear3(x)
+        return x
--- train.py
+++ train.py
@@ -1,44 +1,49 @@
 import torch
+from torch.nn.functional import relu
 import numpy as np
 
-def loss(type,y,y_pred):
-    if type == 'mean_squared':
-        loss = pow(y_pred-y,2).mean()
-    elif type == 'hinge':
-        loss = max(0, 1-torch.sign(y)*y_pred).mean()
-    return loss
+def mse(y_pred, y):
+    return pow(y_pred-y,2).mean()
 
-def sgd_step(dt,bs,xtr,ytr,loss_type,model):
-    index = np.random.choice(xtr.size(0),bs,replace=False)
+def hinge(y_pred, y):
+    return relu(1-y_pred*y).mean()
+
+def loss_fun(type):
+    if type == 'mean_squared':
+        return mse
+    elif type == 'hinge':
+        return hinge
+
+def sgd_step(dt,bs,xtr,ytr,loss,model, gen, replacement=False):
+    if replacement:
+        index = torch.randint(len(xtr), size=(bs,), generator=gen)
+    else:
+        index = torch.randperm(len(xtr), generator=gen)[:bs]
+
     x = xtr[index]
     y = ytr[index]
-    if model == 'linear':
-        y_pred = linear(x,w_1)
-        Loss = loss(loss_type,y,y_pred)
-        Loss.backward()
-        w_1 -= dt * w_1.grad
-    elif model == 'diagonal_linear':
-        y_pred = diagonal_linear(x,w_1,w_2)
-        Loss = loss(loss_type,y,y_pred)
-        Loss.backward()
-        w_1 -= dt * w_1.grad
-        w_2 -= dt * w_2.grad
 
-def train_model(dt,bs,xtr,ytr,loss_type,model):
+    y_pred = model(x)
+    loss_batch = loss(y_pred, y)
+    
+    model.zero_grad()
+    loss_batch.backward()
+
+    for p in model.paramters():
+        p._add(-dt * p.grad())
+
+
+def train_model(dt,bs,xtr,ytr,loss_type,model, replacement, **args):
+    gen = torch.Generator(device="cpu").manual_seed(args['seed_batch'])
+    loss = loss_fun(loss_type)
     nb_iterations = 1000
     data = []
-    w_1 = torch.empty(xtr.size(1)).normal_(0,1).requires_grad_()
-    w_2 = torch.empty(xtr.size(1)).normal_(0,1).requires_grad_()
+
     for i in range(nb_iterations):
-        w_1.grad.data.zero_()
-        w_2.grad.data.zero_()
-        sgd_step(dt,bs,xtr,ytr,loss_type,model)
-        if model == 'linear':
-            y_pred = linear(x,w_1)
-            Ltr = loss(loss_type,ytr,y_pred)
-        elif model == 'diagonal_linear':
-            y_pred = diagonal_linear(x,w_1,w_2)
-            Ltr = loss(loss_type,ytr,y_pred)
+        sgd_step(dt,bs,xtr,ytr,loss,model, gen, replacement)
+        
+        y_pred = model(xtr)
+        Ltr = loss(y_pred, ytr)
         data.append(Ltr)
     return data
     
