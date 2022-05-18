fa39cd7b3f58e817801835d7105ccd4c81627c74
Kanlai Peng <kanlai.peng@epfl.ch>
2022-05-19 00:34:41 +0200

    experiments

--- read_data.py
+++ read_data.py
@@ -10,13 +10,13 @@ objects5 = []
 objects6 = []
 objects7 = []
 objects8 = []
-file_name1 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs128"
-file_name2 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs64"
-file_name3 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs32"
-file_name4 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs16"
-file_name5 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs8"
-file_name6 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs4"
-file_name7 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs2"
+file_name1 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e3"
+file_name2 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e2"
+file_name3 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e1"
+file_name4 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e0"
+file_name5 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e-1"
+file_name6 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e-2"
+file_name7 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/alpha10e-3"
 file_name8 = "C:/Users/Pengkanlai/Desktop/TP4_PCSL_2022/data/expbs1"
 
 with (open(file_name1, "rb")) as f:
@@ -78,28 +78,51 @@ with (open(file_name8, "rb")) as f:
 #nb_steps = len(objects[1]['step'])
 
 
-x = objects1[1]['t']
+
+
+
+zeros_1 = np.zeros(110)-1
+zeros_2 = np.zeros(50)-1
+zeros_3 = np.zeros(10)-1
+zeros_4 = np.zeros(5)-1
+
+x = objects5[1]['t']
 y1 = objects1[1]['dw']
+for i in range(110):
+    y1.append(np.zeros(1)-1)
+#y1 = np.concatenate((y1,zeros_1),axis=0)
 y2 = objects2[1]['dw']
+for i in range(50):
+    y2.append(np.zeros(1)-1)
+#y2 = np.concatenate((y2,zeros_2),axis=0)
 y3 = objects3[1]['dw']
+for i in range(10):
+    y3.append(np.zeros(1)-1)
+#y3 = np.concatenate((y3,zeros_3),axis=0)
 y4 = objects4[1]['dw']
+for i in range(5):
+    y4.append(np.zeros(1)-1)
+#y4 = np.concatenate((y4,zeros_4),axis=0)
 y5 = objects5[1]['dw']
 y6 = objects6[1]['dw']
 y7 = objects7[1]['dw']
-y8 = objects8[1]['dw']
+#y8 = objects8[1]['Train_aloss']
+
 
 #z = objects[1]['Test_error']
 
-plt.plot(x,y1)
-plt.plot(x,y2)
-plt.plot(x,y3)
-plt.plot(x,y4)
-plt.plot(x,y5)
-plt.plot(x,y6)
-plt.plot(x,y7)
-plt.plot(x,y8)
+plt.plot(x,y1,'.')
+plt.plot(x,y2,'.')
+plt.plot(x,y3,'.')
+plt.plot(x,y4,'.')
+plt.plot(x,y5,'.')
+plt.plot(x,y6,'.')
+plt.plot(x,y7,'.')
+plt.title('Linear model with different values of alpha')
+#plt.plot(x,y8)
 #plt.xlim(-400,1000)
+plt.ylim(0,6500)
 plt.xlabel('Time')
 plt.ylabel('Weight variation')
-plt.legend(['bs=128','bs=64','bs=32','bs=16','bs=8','bs=4','bs=2','bs=1'])
+plt.legend(['alpha=1000','alpha=100','alpha=10','alpha=1','alpha=0.1','alpha=0.01','alpha=0.001'])
 plt.show()
